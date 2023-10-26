38b5b1d38deb8cdd6874078966bca2f931f2e370
Christian Cuesta <christianluminor@gmail.com>
2023-10-26 17:02:44 +0200

    Main project done

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,58 @@ void main() {
   runApp(const MainApp());
 }
 
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Recuperar el valor d\'un camp'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: myController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text(myController.text),
+              );
+            },
+          );
+        },
+        tooltip: 'Mostrar el valor del camp',
+        child: const Icon(Icons.text_fields),
+      ),
+    );
+  }
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: MyCustomForm(),
     );
   }
 }
